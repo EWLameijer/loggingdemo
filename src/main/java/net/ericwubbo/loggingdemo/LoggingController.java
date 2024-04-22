54edf39d5bb530c2a06ce6adcc3f5716551f97fd
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-04-22 08:25:48 +0200

    Logback only

--- src/main/java/net/ericwubbo/loggingdemo/LoggingController.java
+++ src/main/java/net/ericwubbo/loggingdemo/LoggingController.java
@@ -4,13 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.logging.*;
 
 @RestController
 public class LoggingController {
-
-    java.util.logging.Logger nativeLogger = java.util.logging.Logger.getLogger("net.ericwubbo.loggingdemo;");
-
     Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
     @RequestMapping
@@ -21,14 +17,6 @@ public class LoggingController {
         logger.info("An INFO Message");
         logger.warn("A WARN Message");
         logger.error("An ERROR Message");
-
-        nativeLogger.finest("A (native) FINEST Message");
-        nativeLogger.finer("A (native) FINER Message");
-        nativeLogger.fine("A (native) FINE Message");
-        nativeLogger.config("A (native) CONFIG Message");
-        nativeLogger.info("An (native) INFO Message");
-        nativeLogger.warning("A (native) WARNING Message");
-        nativeLogger.severe("A (native) SEVERE Message");
 
         return "Howdy! Check out the Logs to see the output...";
     }
